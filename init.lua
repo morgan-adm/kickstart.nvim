9f1aefa1f06eaa08db5c2ad129d7d6c22dbbcfc3
Morgan Moore <morgan@americandigital.marketing>
2024-11-22 16:27:08 -0500

    Add vim-rhubarb for github integration

--- init.lua
+++ init.lua
@@ -955,6 +955,11 @@ require('lazy').setup({
     },
     config = function() end,
   },
+  {
+    'tpope/vim-rhubarb',
+    opts = {},
+    config = function() end,
+  },
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
