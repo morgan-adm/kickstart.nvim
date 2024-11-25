8b1ad31fc3bc1735821a80169a4652fdcf96e2f9
Morgan Moore <morgan@americandigital.marketing>
2024-11-25 15:05:49 -0500

    Fix typo

--- init.lua
+++ init.lua
@@ -457,7 +457,7 @@ require('lazy').setup({
     'neovim/nvim-lspconfig',
     dependencies = {
       -- Automatically install LSPs and related tools to stdpath for Neovim
-      { 'williamboman/mason.nvim', config = true }, -- NOTE: Must be loaded before dependants
+      { 'williamboman/mason.nvim', config = true }, -- NOTE: Must be loaded before dependents
       'williamboman/mason-lspconfig.nvim',
       'WhoIsSethDaniel/mason-tool-installer.nvim',
 
