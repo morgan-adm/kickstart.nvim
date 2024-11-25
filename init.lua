fba07219298470078d1188ee9cb649dd4ac41fe2
Morgan Moore <morgan@americandigital.marketing>
2024-11-25 15:06:52 -0500

    Add borders to hover window

--- init.lua
+++ init.lua
@@ -685,6 +685,23 @@ require('lazy').setup({
           end,
         },
       }
+      -- add borders to floating windows
+      local border = {
+        { '╭', 'NormalFloat' },
+        { '─', 'NormalFloat' },
+        { '╮', 'NormalFloat' },
+        { '│', 'NormalFloat' },
+        { '╯', 'NormalFloat' },
+        { '─', 'NormalFloat' },
+        { '╰', 'NormalFloat' },
+        { '│', 'NormalFloat' },
+      }
+      local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+      function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+        opts = opts or {}
+        opts.border = opts.border or border
+        return orig_util_open_floating_preview(contents, syntax, opts, ...)
+      end
     end,
   },
 
